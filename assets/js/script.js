e880dcf5129d8016809456632b57db955d4fe0ff
Natalia <natalia@MacBook-Air-de-Natalia.local>
2021-06-22 00:47:26 -0400

    ajustes de js, y estructura en general...no pude resolver el collapse

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
-
+    //Smooth scroll_ código de Kodetop
+    $('html, body').animate({
+        scrollTop: $('section').offset().top
+      }, 550);
+    });
+    //Smooth scroll_ código de Kodetop
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
     });
-
+    //Código bootstrap5 - función collpase
+    var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+    var collapseList = collapseElementList.map(function (collapseEl) {
+    return new bootstrap.Collapse(collapseEl)
+    });
     
-
-});
