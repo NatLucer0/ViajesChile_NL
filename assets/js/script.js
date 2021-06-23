fd5bd83d74cd7bdd4e6a02989b3b73610d4a54d8
Natalia <natalia@MacBook-Air-de-Natalia.local>
2021-06-22 22:45:27 -0400

    collapse en iconos quienes somos -logro desbloqueado

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,22 @@ $(document).ready(function(){
         scrollTop: $('section').offset().top
       }, 550);
     });
+    console.log($(window).width());
+   if ($(window).width() < 767) {
+     $('.icono').each(function(){
+       $(this).attr('aria-expanded','false'); 
+     })
+   }
+   else{  $('.icono').each(function(){
+    $(this).attr('aria-expanded','true'); 
+  })}
     //TOOLTIP
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
     });
+    var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+    var collapseList = collapseElementList.map(function (collapseEl) {
+    return new bootstrap.Collapse(collapseEl)
+
+})
